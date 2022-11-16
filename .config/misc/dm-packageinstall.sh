fd3ae4cbb66489612c6c52568b346d8888d0923d
hp27596 <hp27596@gmail.com>
2022-11-16 12:49:00 +0700

    New backup 2022-11-16 12:49:00

--- .config/misc/dm-packageinstall.sh
+++ .config/misc/dm-packageinstall.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env sh
 
+terminal='alacritty' #replace with your terminal of choice
 action=$(echo -e "1. Install a Package\n2. Uninstall a Package" | dmenu -i -l 2 -p 'Choose action:')
 if [[ $action == *"1"* ]]; then
     notify-send "Querying Package Database"
     choice=$(yay -Slq | dmenu -i -l 20 -p 'Choose package:')
 
-    if [ $choice ]; then
+    if [[ $choice ]]; then
         notify-send "Updating system and installing package $choice"
-        alacritty --hold -e sh -c "yay -Syu $choice && $SHELL"
+        $terminal --hold -e sh -c "yay -Syu $choice && $SHELL"
     fi
 elif [[ $action == *"2"* ]]; then
     notify-send "Listing All User Installed Packages"
     sleep 1
     choice=$(yay -Qqe | dmenu -i -l 20 -p 'Choose package:')
 
-    if [ $choice ]; then
+    if [[ $choice ]]; then
         notify-send "Uninstalling package $choice"
-        alacritty --hold -e sh -c "yay -Rns $choice ; $SHELL"
+        $terminal --hold -e sh -c "yay -Rns $choice ; $SHELL"
     fi
 
 fi
